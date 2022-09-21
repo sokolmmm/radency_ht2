f6f3d0b0f1f3f0157f6e4189e2048b7f1dfa1bba
Sokol Misha <sokol.misha.m.com>
2022-09-21 12:30:44 +0300

    Add routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index element={<div>Home</div>} />
+        <Route path="/note" element={<div>Outlet</div>}>
+          <Route path="create" element={<div>Create</div>} />
+          <Route path="edit" element={<div>Edit</div>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
